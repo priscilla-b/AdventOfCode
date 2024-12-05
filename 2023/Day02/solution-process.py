9d985b12ab3e96fa058f0d6334e75c1ee31024f5
Priscilla Baah <pyfbaah@gmail.com>
2024-12-05 18:35:24 +0000

    update

--- 2023/Day02/solution-process.py
+++ 2023/Day02/solution-process.py
@@ -3,51 +3,122 @@ from pathlib import Path
 import re
 
 
-# f = open('input/sample-input.txt', mode="rt")
 
-# 'r' means read. 't' means text.
-# 'rt' means open for reading text. default mode, so not necessary to specify
-#  modes are important because it prevents your program from making unexpected
-#  changes to your program. E.g. your program modifying a file you only intended to read
-# input_data = f.read().splitlines()
-# for line in input_data:
-#     pass
-#     # print(line)
-# f.close()
-
-# using Pathlib to read file paths ensures your program is agnostic of 
-# operating systems
 SCRIPT_DIR = Path(__file__).parent  # get the directory program is running from
 
-INPUT_PATH = Path(SCRIPT_DIR, 'input/sample-input.txt')
+INPUT_PATH = Path(SCRIPT_DIR, 'input/input.txt')
 
-{
-    "blue":12,
-    "green":13,
-    "blue":14
-}
+# cubes_dict = {
+#     "blue":14,
+#     "green":13,
+#     "red":12
+# }
+
+# allowed_colors = set(cubes_dict.keys())
+
+
+# with open (INPUT_PATH) as f:
+#     input_data = f.read().splitlines()
+
+# total_games = 0
+# sum_impossible_games = 0
+
+# for text in input_data:
+    
+#     split_pattern = r'[:;,]'
+#     text_contents = re.split(split_pattern, text)
+
+#     game_id = int(text_contents[0].split(' ')[1])
+    
+#     total_games += game_id
+
+#     picks = text_contents[1:]
+
+#     for pick in picks:
+#         pick_info = pick.strip().split(' ')
+
+#         if len(pick_info) != 2:
+#             print(f"Error in input format in Game {game_id}")
+#             continue
+
+#         value = int(pick_info[0])
+#         color = pick_info[1].lower()
+
+
+#         if color not in allowed_colors or value > cubes_dict[color]:
+#             sum_impossible_games += game_id
+#             break
+        
+    
+# sum_possible_games  = total_games - sum_impossible_games
+    
+
+# print("sum possible games: ", sum_possible_games)
+    
+
+
+# part two: fewest number of cubes needed in the bag at a time to make a game possible
+
 
 
 with open (INPUT_PATH) as f:
     input_data = f.read().splitlines()
 
-    sum_possible_games = 0
-    for text in input_data:
-        split_pattern = r'[:;,]'
-        text_contents = re.split(split_pattern, text)
-        # text_contents = text.split(":");
-        # game_num = text_contents[0][-1]
-        # games = text_contents[1].split(";")
-        # for game in games: 
-        #     print("game ", game_num, game)
-
-        print(text_contents[0][-1], text_contents[1:])
-    
+sum_power_set = 0
 
 
-     
 
+for text in input_data:
+    split_pattern = r'[:;,]'
+    text_contents = re.split(split_pattern, text)
 
-                
+    picks = text_contents[1:]
+
+    power_set = 1
+
+    cubes_dict = {
+        "blue":0,
+        "green":0,
+        "red":0
+    }
+
+    for pick in picks:
+        pick_info = pick.strip().split(' ')
+
+       
+        value = int(pick_info[0])
+        color = pick_info[1].lower()
+
+        try:
+            color_value = cubes_dict[color]
+
+            if value > color_value:
+                cubes_dict[color] = value
+
+        except(KeyError):
+            print("color does not exist in cube dictionary")
+            continue
+
         
+
+    for value in cubes_dict.values():
+        power_set *= value
+
+    sum_power_set += power_set
     
+        
+        
+
+print("sum power set: ", sum_power_set)
+    
+
+
+
+
+
+    
+
+
+            
+    
+
--- 2023/Day02/solution.py
+++ 2023/Day02/solution.py
@@ -6,32 +6,20 @@ Solving https://adventofcode.com/2023/day/1
 Using template from https://aoc.just2good.co.uk/python/my-aoc-template
 
 Problem:
-    Given a document with a list of text. Find the sum of all calibration
-    values in the document. A calibration value can be derived from each line of text
-    by forming a two digit number with the first and last digit(in that order) in the text.
+    Given a document with a list of text where each line of text represent a game played, find the sum
+    of all possible games. A game consists of a combination of red, blue and green cubes
 
 Solution Overview:
 Part 1:
-    In solution 1, the idea is to iterate through all the characters in each line and 
-    check if they're digits, then get the first and last digits.
-    A digit list was created and a character was marked as a digit if they're present in the digit list
 
-    There are two ways to get the first and last digits:
-        1. create a list of all digits and use list indexes 0 and -1
-        to get the first and list digits respectively. Illustrated in the
-        part_one_with_list() function
-        
-        2. avoid storing a list of digits by creating two variables to store the first
-        and last digits. Both variables are initialized as None. Check if a character is a digit
-        and then set it as the first digit if the first digit is still None. If a first digit has been 
-        assigned, then assign it as the last digit. This process is repeated for all character in a given
-        line of text. At the end of the iteration, check if the last digit is still None and set its value
-        to that of the first digit. This accounts for when a text contains only one digit
+Part 2:
+
 
 """
 
 from pathlib import Path
 import time
+import re
 from Common.common import get_logger
 
 log = get_logger()
@@ -40,88 +28,57 @@ SCRIPT_DIR = Path(__file__).parent
 
 INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
 
-DIGITS = [str(i) for i in range(10)]
+cubes_dict = {
+    "blue":14,
+    "green":13,
+    "red":12
+}
 
-DIGITS_DICT = {
-    'one':'1',
-    'three':'3',
-    'two':'2',
-    'four':'4',
-    'five':'5',
-    'six':'6',
-    'seven':'7',
-    'eight':'8',
-    'nine':'9'
-    }
+allowed_colors = set(cubes_dict.keys())
+
 
 
 def main():
     
 
-    def part_one_with_list():
-        sum_of_digits = 0
-
-        log.info("with list")
-        with open (INPUT_FILE) as f:
-            input_data = f.read().splitlines()
-            for text in input_data:
-                input_digits = [x for x in text if x in DIGITS]
-
-                try:
-                    first = input_digits[0]
-                except IndexError:
-                    first = 0
-                try:
-                    last = input_digits[-1]
-                except IndexError:
-                    last = 0
-
-               
-                two_digits = int(first + last)
-               
-
-                sum_of_digits += two_digits
-            
-                # log.debug('%s : %s', text, two_digits)
-
-        log.debug('sum_of_digits: %s', sum_of_digits)
-
-    def part_one_without_list():
-
-        log.info("without list")
-
-        sum_of_digits = 0
+    def part_one():
+        
         with open (INPUT_FILE) as f:
             input_data = f.read().splitlines()
 
+        total_games = 0
+        sum_impossible_games = 0
 
         for text in input_data:
-            first = None
-            last = None
-            for char in text:
-                if char in DIGITS:
+    
+            split_pattern = r'[:;,]'
+            text_contents = re.split(split_pattern, text)
+
+            game_id = int(text_contents[0].split(' ')[1])
             
-                    if not first:
-                        first = char
-                        continue
+            total_games += game_id
 
-                    if first:
-                        last = char
-                        continue
+            picks = text_contents[1:]
 
-            if first and not last:
-                last = first
+            for pick in picks:
+                pick_info = pick.strip().split(' ')
 
-            try:
-                    two_digits = int(first + last)
-            except TypeError:
-                two_digits = 0
+                if len(pick_info) != 2:
+                    print(f"Error in input format in Game {game_id}")
+                    continue
 
-            sum_of_digits += two_digits
+                value = int(pick_info[0])
+                color = pick_info[1].lower()
 
-            # log.debug('%s : %s', text, two_digits)
 
-        log.debug('sum_of_digits: %s', sum_of_digits)
+                if color not in allowed_colors or value > cubes_dict[color]:
+                    sum_impossible_games += game_id
+                    break
+            
+    
+        sum_possible_games  = total_games - sum_impossible_games
+        log.debug('sum possible games: %s', sum_possible_games)
+
 
 
 
@@ -130,56 +87,15 @@ def main():
         log.info("part two")
 
 
-        sum_of_digits = 0
-        with open (INPUT_FILE) as f:
-            input_data = f.read().splitlines()
-
-
-        for text in input_data:
-            digit_text = text.lower()
-
-            for k, v in DIGITS_DICT.items():
-                digit_text =  digit_text.replace(k, k[0]+v+k[-1])
-                # to account for overlapping digits when replace a 
-                # digit text include the first and last digits
-
-            first = None
-            last = None
-            for char in digit_text:
-                if char in DIGITS:
-            
-                    if not first:
-                        first = char
-                        continue
-
-                    if first:
-                        last = char
-                        continue
-
-            if first and not last:
-                last = first
-
-            try:
-                    two_digits = int(first + last)
-            except TypeError:
-                two_digits = 0
-
-            sum_of_digits += two_digits
-
-            # log.debug('%s : %s', text, two_digits)
-
-        log.debug('sum_of_digits: %s', sum_of_digits)
+        
 
     t1 = time.perf_counter()
-    part_one_with_list()
+    part_one()
     t2 = time.perf_counter()
     log.info("Execution time: %0.4f seconds", t2 - t1)
         
 
-    t1 = time.perf_counter()
-    part_one_without_list()
-    t2 = time.perf_counter()
-    log.info("Execution time: %0.4f seconds", t2 - t1)
+ 
 
     t1 = time.perf_counter()
     part_two()
